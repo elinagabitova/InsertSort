e24fff3cf8c2bbc5da3a2678b5072d42c2318922
elinagabitova <elinagabitovaa@gmail.com>
2017-05-21 18:57:12 +0500

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -18,7 +18,7 @@ SCENARIO("matr")
   int arr[3][3] = {4, 21, 12, 5, 2, 7, 42, 26, 8};
   int * beg = &arr[0][0];
   int * end = &arr[2][3];
-  insertion_sort(beg,end);
+  insertsort(beg,end);
   REQUIRE(arr[0][0] == 2);
   REQUIRE(arr[0][1] == 4);
   REQUIRE(arr[0][2] == 5);
